f50036b402cb5a700a94ad0f828a0d33f896475e
Weihan Li <weihanli@outlook.com>
2024-01-11 23:53:54 +0800

    Create build.cs
    [skip ci]

---
+++ scripts/build.cs
@@ -0,0 +1,21 @@
+var message = """
+The `weihanli/mdnice` docker image has been updated
+[Amazing]
+""";
+  
+var webhookUrl = Environment.GetEnvironmentVariable("DingBotWebhookUrl");
+if (string.IsNullOrEmpty(webhookUrl))
+{
+    Console.WriteLine("WebhookUrl not found");
+    return;
+}
+
+using var response = await HttpHelper.HttpClient.PostAsJsonAsync(webhookUrl, new
+    {
+        msgtype = "text",
+        text = new
+        {
+            content = message
+        }
+    });
+response.EnsureSuccessStatusCode();
